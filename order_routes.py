879e5b290a316789c55b17ed8939091f2ce81518
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2026-02-16 16:02:36 -0300

    criando Niveis de autorização

--- order_routes.py
+++ order_routes.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, HTTPException, Depends
-from dependencies import pegar_sessao
+from dependencies import pegar_sessao, verificar_token
 from sqlalchemy.orm import Session
 from models import Pedido
 from schemas import PedidoSchema
 
 ## ESSE é o meu prefixo do meu router, ou seja, todas as rotas que eu criar aqui dentro do auth_router vão começar com /pedidos
-order_router = APIRouter(prefix="/pedidos", tags=["pedidos"])
+order_router = APIRouter(prefix="/pedidos", tags=["pedidos"], dependencies=[Depends(verificar_token)])
 
 ## ESSE é o meu endpoint, ou seja, a rota completa para acessar essa função vai ser /pedidos/lista
 @order_router.get("/")
@@ -23,3 +23,14 @@ async def criar_pedido( pedido_schema: PedidoSchema, session: Session = Depends(
     session.refresh(novo_pedido)
 
     return {"msg": f"Pedido criado com sucesso, ID: {novo_pedido.id}"}
+
+
+@order_router.post("/pedido/cancelar/{id_pedido}")
+async def cancelar_pedido(id_pedido: int, session: Session = Depends(pegar_sessao)):
+    pedido = session.query(Pedido).filter(Pedido.id == id_pedido).first()
+    if not pedido:
+        raise HTTPException(status_code=404, detail="Pedido não encontrado")
+    pedido.status == "cancelado"
+    session.commit()
+    return {"msg": f"Pedido cancelado com sucesso, ID: {id_pedido}"}    
+    
